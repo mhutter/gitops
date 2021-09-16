34f780c81e49f33bb5a13b4e83181c3e612d5d1e
Manuel Hutter <manuel@hutter.io>
2021-09-16 08:49:28 +0200

    Combine kubernets/kubelet upgrade plans
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

---
+++ system-upgrade/upgrade-k8s.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -e -u -o pipefail -x
+
+target_version="${SYSTEM_UPGRADE_PLAN_LATEST_VERSION#v}"
+target_minor="$(echo "$target_version" | cut -d. -f1-2)"
+
+echo \
+  "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/${target_minor}/xUbuntu_20.04/ /" \
+  > /etc/apt/sources.list.d/crio.list
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y --allow-change-held-packages \
+  "kubeadm=${target_version}-00" \
+  "kubelet=${target_version}-00" \
+  "kubectl=${target_version}-00"
+apt-mark hold kubeadm kubelet kubectl
+
+# Upgrade cluster itself if required
+current_version="$(kubectl version --short | awk '/^Server/{print $3}')"
+if [[ "$current_version" != "$target_version" ]]; then
+  kubeadm upgrade plan "$SYSTEM_UPGRADE_PLAN_LATEST_VERSION"
+  kubeadm upgrade apply --yes "$SYSTEM_UPGRADE_PLAN_LATEST_VERSION"
+  KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply \
+    -f https://docs.projectcalico.org/manifests/calico.yaml
+fi
+
+kubeadm upgrade node
+
+old_pid="$(pgrep kubelet)"
+systemctl daemon-reload
+systemctl restart kubelet
+
+while [[ "$(pgrep kubelet)" == "$old_pid" ]]; do sleep 1; done
+while ! curl -sSk -m1 https://127.0.0.1:10250/healthz; do sleep 1; done
