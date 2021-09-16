d1aa71fbb3ce3a6d8120b688d7df056204b1f044
Manuel Hutter <manuel@hutter.io>
2021-09-16 09:06:27 +0200

    Ensure previous K8s version can be determined
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- system-upgrade/upgrade-k8s.sh
+++ system-upgrade/upgrade-k8s.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e -u -o pipefail -x
 
+export KUBECONFIG=/etc/kubernetes/admin.conf
+export DEBIAN_FRONTEND=noninteractive
+
 target_version="${SYSTEM_UPGRADE_PLAN_LATEST_VERSION#v}"
 target_minor="$(echo "$target_version" | cut -d. -f1-2)"
 
@@ -8,7 +11,6 @@ echo \
   "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/${target_minor}/xUbuntu_20.04/ /" \
   > /etc/apt/sources.list.d/crio.list
 
-export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y --allow-change-held-packages \
   "kubeadm=${target_version}-00" \
@@ -21,7 +23,7 @@ current_version="$(kubectl version --short | awk '/^Server/{print $3}')"
 if [[ "$current_version" != "$target_version" ]]; then
   kubeadm upgrade plan "$SYSTEM_UPGRADE_PLAN_LATEST_VERSION"
   kubeadm upgrade apply --yes "$SYSTEM_UPGRADE_PLAN_LATEST_VERSION"
-  KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply \
+  kubectl apply \
     -f https://docs.projectcalico.org/manifests/calico.yaml
 fi
 
